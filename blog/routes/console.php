b27f2684f5a854fdc4369742bf595fcc003d76ef
BENYEKHLEF-Anouar <benyekhlef.anouar.solicode@gmail.com>
2025-10-14 13:45:07 +0100

    Tutorial completed

--- blog/routes/console.php
+++ blog/routes/console.php
@@ -6,3 +6,19 @@ use Illuminate\Support\Facades\Artisan;
 Artisan::command('inspire', function () {
     $this->comment(Inspiring::quote());
 })->purpose('Display an inspiring quote');
+
+
+// composer install
+// copy .env.example .env
+// php artisan key:generate
+
+// DB_CONNECTION=mysql
+// DB_HOST=127.0.0.1
+// DB_PORT=3306
+// DB_DATABASE=blog
+// DB_USERNAME=root
+// DB_PASSWORD=
+
+// php artisan migrate
+
+
